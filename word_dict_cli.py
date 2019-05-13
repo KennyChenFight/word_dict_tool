a271bd6d21708947782070956ea28975d22939cb
KennyChen <kennychen851228@gmail.com>
2019-05-13 12:08:33 +0800

    做cli介面供使用

---
+++ word_dict_cli.py
@@ -0,0 +1,10 @@
+import fire
+from dictionary import WordDictionary
+
+# search_sentence: search_sentence 華仔
+# mark_phonetic: ['"hu aL aH"','"tzL tzL"']
+# add_sentence_to_word_dict: 華仔 ['"hu aL aH"','"tzL tzL"']
+#
+
+if __name__ == '__main__':
+    fire.Fire(WordDictionary)
